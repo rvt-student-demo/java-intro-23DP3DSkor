f5f112bce7071e4843e840e76df9a1fdbbfb2836
23DP3DSkor <166013323+23DP3DSkor@users.noreply.github.com>
2024-10-16 07:25:56 +0000

    Second plus third

--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -1,24 +1,44 @@
 package lv.rvt;
 
 public class Stars{
-    public static void printStars(int number){
-        for( int i = 0; i < number; i++){
-            System.out.print("*");
-        }
-        System.out.println();
-    }
-    public static void printSqure(int size){
-        for(int i = 0; i < size; i++){
-            printStars(size);
-        }
-    }
-    private static void printRectangle(int width, int height){
-        int i = 0;
-         while(i < height){
-           int j = 0;
-             while(j < width){
-               printStars(width);
-               j++;
-       }
+//     public static void printStars(int number){
+//         for( int i = 0; i < number; i++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+//     public static void printSqure(int size){
+//         for(int i = 0; i < size; i++){
+//             printStars(size);
+//         }
+//     }
+//     public static void printRectangle(int width, int height){
+//         int i = 0;
+//          while(i < height){
+//            int j = 0;
+//              while(j < width){
+//                printStars(width);
+//                j++;
+//        }
+//     }
+// }
+    // public static void printSpaces(int number){
+    //     for (int i = 0; i < number; i++){
+    //         System.out.println(" ");
+    //     }
+    // }
+    // public static void printStars(int number){
+    //     for (int i =  0; i < number; i++){
+    //         System.out.println("*");
+    //     }
+    // }
+    // public static void printTriangle(int size){
+    //     for (int i = 1; i <= size; i++){
+    //         printSpaces(size - i);
+    //         printSpaces(i);
+    //         System.out.println();
+            
+    //     }
     }
 }
+
